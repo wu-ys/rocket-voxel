b4999192930e83890c2b6bde4b3bdddb336b04cd
wu-ys <wu-ys20@mails.tsinghua.edu.cn>
2022-11-29 11:47:13 +0800

    updates

--- main.py
+++ main.py
@@ -13,13 +13,13 @@ char_d = ti.Matrix(chard)
 
 @ti.func
 def draw_cylinder(pos, radius, height, color, color_noise):
-    for I in ti.grouped(ti.ndrange((-radius, radius), (0, height), (-radius, +radius))):
+    for I in ti.grouped(ti.ndrange((-radius, 1+radius), (0, height), (-radius, 1+radius))):
         if (I[0]**2 + I[2]**2 <= radius**2):
             scene.set_voxel(pos+I, 1, color + color_noise * ti.random())
 
 @ti.func
 def draw_cone(pos, radius, height, color, color_noise):
-    for I in ti.grouped(ti.ndrange((-radius, radius), (0, height), (-radius, +radius))):
+    for I in ti.grouped(ti.ndrange((-radius, 1+radius), (0, height), (-radius, 1+radius))):
         if (I[0]**2 + I[2]**2 <= radius**2 * (1- I[1]/height)):
             if (I[1] == 0):
                 scene.set_voxel(pos+I, 1, vec3(0.1, 0.1, 1.0))
@@ -28,8 +28,8 @@ def draw_cone(pos, radius, height, color, color_noise):
 
 @ti.func
 def draw_fire(pos, radius, height, color, color_noise):
-    for I in ti.grouped(ti.ndrange((-2*radius, 2*radius), (0, height), (-2*radius, 2*radius))):
-        if (I[0]**2 + I[2]**2 <= radius**2 * (1+I[1]/height)) and (ti.random() < 1- I[1]/height):
+    for I in ti.grouped(ti.ndrange((-2*radius, 1+2*radius), (0, height), (-2*radius, 1+2*radius))):
+        if (I[0]**2 + I[2]**2 <= radius**2 * (1+3*I[1]/height)) and (ti.random() < (1- I[1]/height)**2):
             scene.set_voxel(vec3(pos[0]+I[0], pos[1]-I[1], pos[2]+I[2]), 20, color + color_noise * ti.random()) 
 
 @ti.kernel
@@ -39,7 +39,6 @@ def initialize_voxels():
     draw_cylinder(pos=(-15,0,0), radius=5, height=50, color=(1, 1, 1), color_noise=vec3(0.1))
     draw_cylinder(pos=(0,0,15), radius=5, height=50, color=(1, 1, 1), color_noise=vec3(0.1))
     draw_cylinder(pos=(0,0,-15), radius=5, height=50, color=(1, 1, 1), color_noise=vec3(0.1))
-
     draw_cone(pos=(0,150,0), radius=10, height=30, color=(1,1,1), color_noise=vec3(0.1))    
     draw_cone(pos=(15,50,0), radius=5, height=15, color=(1,1,1), color_noise=vec3(0.1))    
     draw_cone(pos=(-15,50,0), radius=5, height=15, color=(1,1,1), color_noise=vec3(0.1))    
@@ -52,13 +51,14 @@ def initialize_voxels():
     for key in ti.static(range(26)):
         scene.set_voxel(vec3(-4+char_d[key,1], 114+char_d[key,0], 10), 1, vec3(0.1, 0.1, 1.0))
     # draw flag
-    for I in ti.static(ti.ndrange((-4,4), (-3,3))):
+    for I in ti.static(ti.ndrange((-4,5), (-3,3))):
         scene.set_voxel(vec3(I[0], I[1]+106, 10), 1, vec3(1.0, 0.1, 0.1))
+        
     draw_fire(vec3(0,0,0), 10, 120, vec3(0.8, 0.4, 0.0), vec3(0.1))
     draw_fire(vec3(15,0,0), 5, 80, vec3(0.8, 0.4, 0.0), vec3(0.1))
     draw_fire(vec3(0,0,15), 5, 80, vec3(0.8, 0.4, 0.0), vec3(0.1))
     draw_fire(vec3(-15,0,0), 5, 80, vec3(0.8, 0.4, 0.0), vec3(0.1))
     draw_fire(vec3(0,0,-15), 5, 80, vec3(0.8, 0.4, 0.0), vec3(0.1))
-    
+
 initialize_voxels()
 scene.finish()
